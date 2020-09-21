8a7911d3a7a75f6e39b143258a745c8ef2205647
ben39053372 <ben39053372@gmail.com>
2020-09-21 17:44:37 +0800

    add createUser func

--- src/repository/user.repository.ts
+++ src/repository/user.repository.ts
@@ -1,10 +1,12 @@
 import { EntityRepository, Repository } from 'typeorm'
 import UserModel from '../models/user.model'
 import { Service } from 'typedi'
+import { IUserInputDTO } from '../interfaces/user.t'
 
 @Service()
 @EntityRepository(UserModel)
 export default class UserRepository extends Repository<UserModel> {
+
   async findByUsername(username: string): Promise<UserModel> {
     return await this.findOneOrFail({ username })
   }
@@ -16,5 +18,13 @@ export default class UserRepository extends Repository<UserModel> {
         { username }
       ]
     })
+  }
+
+  async createUser(userDTO: IUserInputDTO) {
+    let newUser = this.create()
+    newUser.email = userDTO.email;
+    newUser.username = userDTO.username;
+    newUser.password = userDTO.password;
+    return await this.save(newUser)
   }
 }
