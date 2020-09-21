4dff2a61e1e31aa11e952f411bcb5cb1d64e04ca
ben39053372 <ben39053372@gmail.com>
2020-09-21 17:45:01 +0800

    mark useRepository as a service

--- src/repository/item.repository.ts
+++ src/repository/item.repository.ts
@@ -1,6 +1,8 @@
 import { EntityRepository, Repository } from 'typeorm'
 import ItemModel from '../models/item.model'
+import { Service } from 'typedi'
 
+@Service()
 @EntityRepository(ItemModel)
 export default class UserRepository extends Repository<ItemModel> {
 
